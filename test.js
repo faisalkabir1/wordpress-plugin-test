bc115b38865f5ea65136f36262537f2a9ddc4efb
Faisal Kabir <onlyfaisalkabir@gmail.com>
2024-04-25 02:05:26 +0600

    Update test.js

--- test.js
+++ test.js
@@ -7,4 +7,20 @@ test('Log in to WordPress site', async t => {
     await t
         .typeText('#user_login', 'faisal')
         .typeText('#user_pass', 'faisal')
-        .click('#wp-submit')})
+        .click('#wp-submit')
+        
+        //Check WP Dark mode Plugin
+        const darkModeMenuItem = Selector('#toplevel_page_wp-dark-mode');
+
+        if (await darkModeMenuItem.exists) {
+            // If the dark mode menu item exists, click on it
+            await t.click(darkModeMenuItem);
+        } else {
+            // If the dark mode menu item doesn't exist, click on the "plugins" and then activate wp dark mode
+            await t
+                .click('#menu-plugins')
+                .click('#activate-wp-dark-mode');
+        }
+    
+        // Proceed with the rest of the test if needed
+    });
