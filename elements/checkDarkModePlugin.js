8370d624e2aa7a066f433867eb2f51d0ba29bff6
Faisal Kabir <onlyfaisalkabir@gmail.com>
2024-04-25 13:23:00 +0600

    modify check dark mode plugin

--- elements/checkDarkModePlugin.js
+++ elements/checkDarkModePlugin.js
@@ -1,14 +1,16 @@
 import { Selector, t } from 'testcafe';
+
 class DarkModePlugin {
     constructor() {
         this.darkModeMenuItem = Selector('#toplevel_page_wp-dark-mode');
-}
+    }
 
-async checkDarkModePlugin() {
-    await this.darkModeMenuItem.exists; {
-        await t.click(this.darkModeMenuItem);
-        await t.wait(3000); // Adjust the wait time as needed
+    async checkDarkModePlugin() {
+        if (await this.darkModeMenuItem.exists) {
+            await t.click(this.darkModeMenuItem);
+            await t.wait(3000);
+        }
     }
 }
-}
+
 export default DarkModePlugin;
--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 import { Selector } from 'testcafe';
 import Login from elements/login;
+import DarkModePlugin from './elements/checkDarkModePlugin';
+import DarkModeHandler from './elements/DarkModeHandler'; 
 fixture`WordPress Site Tests`
     .page`http://localhost/wordpress/wp-login.php`;
 const login = new Login;
@@ -12,7 +14,12 @@ test('Log in to WordPress site', async t => {
         //Check WP Dark mode Plugin
 test('Check Dark Mode Plugin', async t => {
         await login.LoginMethod(username, pass);
-        const darkModeHandler = new DarkModeHandler();
-        await darkModeHandler.checkDarkModePlugin();
+        const darkModePulgincheck = new DarkModePlugin();
+        await darkModePulgincheck.checkDarkModePlugin();
 
     });
+    test ('If Active, navigate to the WP Dark Mode & continue. Otherwise, Install the Plugin and Activate it', async t =>{
+        await login.LoginMethod(username, pass);
+        const darkModeHandler = new DarkModeHandler();
+        await darkModeHandler.checkDarkModePlugin();
+    })
