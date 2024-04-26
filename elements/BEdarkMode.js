b257be3ed320e484d12644ef1c5a37ba6aeeec06
Faisal Kabir <onlyfaisalkabir@gmail.com>
2024-04-27 02:44:42 +0600

    Fix code for admin backend dark mode

--- elements/BEdarkMode.js
+++ elements/BEdarkMode.js
@@ -3,13 +3,14 @@ import { Selector, t } from 'testcafe';
 class BEdarkMode {
     constructor() {
         this.adminDarkMode = Selector('a').withText('Admin Panel Dark Mode');
+        this.settings = Selector('a').withText('Settings');
         this.switchButton = Selector('div.relative.w-10.h-full.rounded-full.transition.duration-100.bg-slate-200');
     }
 
     async BEdarkModeMethod() {
-       
+       await t.click(this.settings);
         await t.click(this.adminDarkMode );
-        await t.wait(1000);
+        await t.wait(2000);
     // Click on the switch button to toggle its state
         await t.click(this.switchButton);
     }
--- elements/SwitchPositionChange.js
+++ elements/SwitchPositionChange.js
@@ -3,10 +3,6 @@ import { Selector, t } from 'testcafe';
 class SwitchPositionLeft {
     constructor() {
        this.leftBtn = Selector('span').withText('Left');
-       this.Accessibility = Selector('a[data-v-571e2e25]')
-       .withAttribute('href', '#/accessibility')
-       
-       .withText('Accessibility');
          }
     async SwitchPositiontoLeft() {
        await t.click(this.leftBtn);
