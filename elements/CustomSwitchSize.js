ddb21813c174479e964ac8223d69cc1044d038de
Faisal Kabir <onlyfaisalkabir@gmail.com>
2024-04-26 23:53:57 +0600

    Create CustomSwitchSize.js

---
+++ elements/CustomSwitchSize.js
@@ -0,0 +1,19 @@
+Selector('div.cursor-pointer.flex.items-center.gap-2.py-2.transition.duration-75.px-3.5.text-base.font-normal.leading-6.rounded-lg.bg-gray-100.hover:bg-gray-200');
+import { Selector, t } from 'testcafe';
+
+class CustomSwitch {
+    constructor() {
+       this.customswitch = Selector('div.cursor-pointer.flex.items-center.gap-2.py-2.transition.duration-75.px-3.5.text-base.font-normal.leading-6.rounded-lg.bg-gray-100.hover:bg-gray-200');
+        this.inputrange = Selector('#ek7fjj12e');
+         }
+    async SwitchChangeMethod() {
+       
+        await t.click(this.customswitch);
+        await t.typeText(this.inputrange),'200';
+       
+        
+    
+    }
+}
+
+export default CustomSwitch;
