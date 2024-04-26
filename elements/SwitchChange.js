cb550f6ed2071a5d1a806f239fa59a2c635ad1c2
Faisal Kabir <onlyfaisalkabir@gmail.com>
2024-04-26 23:33:20 +0600

    Update SwitchChange.js

--- elements/SwitchChange.js
+++ elements/SwitchChange.js
@@ -3,13 +3,14 @@ import { Selector, t } from 'testcafe';
 class SwitchChange {
     constructor() {
         this.SwitchSetting = Selector('a[data-v-571e2e25]').withAttribute('href', '#/switch').withText('Switch Settings');
-        this.switch = Selector('div').hasClass('bg-[#F9FAFB]')
+        this.switch = Selector('div[data-v-039c3d41][data-size="1"].wp-dark-mode-ignore.wp-dark-mode-switch.ignore.dummy.wp-dark-mode-switch-2.dummy');
     }
     async SwitchChangeMethod() {
        
         await t.click(this.SwitchSetting);
-        await t.wait(2000);
+       
         await t.click(this.switch);
+        await t.wait(2000);
     
     }
 }
