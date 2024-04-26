f2c7251423aa13c7bfb7f6cc483f968ed035367b
Faisal Kabir <onlyfaisalkabir@gmail.com>
2024-04-26 23:12:55 +0600

    Update SwitchChange.js

--- elements/SwitchChange.js
+++ elements/SwitchChange.js
@@ -3,12 +3,13 @@ import { Selector, t } from 'testcafe';
 class SwitchChange {
     constructor() {
         this.SwitchSetting = Selector('a[data-v-571e2e25]').withAttribute('href', '#/switch').withText('Switch Settings');
+        this.switch = Selector('div').hasClass('bg-[#F9FAFB]')
     }
     async SwitchChangeMethod() {
        
         await t.click(this.SwitchSetting);
         await t.wait(2000);
-        await t.click(Selector('div').hasClass('bg-[#F9FAFB]'));
+        await t.click(this.switch);
     
     }
 }
