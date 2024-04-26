bc15bbdef54169077c8211c69735872a8392e08e
Faisal Kabir <onlyfaisalkabir@gmail.com>
2024-04-26 18:12:04 +0600

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,8 @@ import Login from './elements/login';
 import DarkModePlugin from './elements/checkDarkModePlugin';
 import DarkModeHandler from './elements/DarkModeHandler'; 
 import BEdarkMode from './elements/BEdarkMode';
-
+import saveChanges from './elements/SaveChangesButton';
+import saveChanges from './elements/SaveChangesButton';
 
 fixture`WordPress Site Tests`
     .page`http://localhost/wordpress/wp-login.php`;
@@ -13,7 +14,7 @@ fixture`WordPress Site Tests`
     const darkModePulgincheck = new DarkModePlugin();
     const darkModeHandler = new DarkModeHandler();
    const backendDarkMode = new BEdarkMode();
-    
+    const SaveChanges = new saveChanges();
     test('Log in to WordPress site', async t => {
     await login.loginToWordPress(username, password);
 })
@@ -26,13 +27,14 @@ test('Check Dark Mode Plugin', async t => {
     });
 test.only('If Active, navigate to the WP Dark Mode & continue. Otherwise, Install the Plugin and Activate it', async t =>{
         await login.loginToWordPress(username, password);
-        await t.wait(3000);
+        await t.wait(2000);
      
         await darkModeHandler.DarkModeHandlerMethod();
         //await darkModePulgincheck.checkDarkModePlugin();
-        await t.wait(2000);
+        await t.wait(1000);
        
         //Enable Backend Darkmode
         await backendDarkMode.BEdarkModeMethod();
-    
+        await t.wait(2000);
+        await SaveChanges.SaveChanges();
     })
